cfcd99e4fe77c4c7a545911144c5808d92b6908a
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-05-10 21:45:21 -0700

    add copy icon button to AccountInfoItem

--- app/src/main/kotlin/app/grapheneos/info/ui/donate/banktransfers/AccountInfoItem.kt
+++ app/src/main/kotlin/app/grapheneos/info/ui/donate/banktransfers/AccountInfoItem.kt
@@ -1,34 +1,65 @@
 package app.grapheneos.info.ui.donate.banktransfers
 
+import android.content.ClipData
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.Wallpapers
 import androidx.compose.ui.unit.dp
+import app.grapheneos.info.R
 
 @Composable
 fun AccountInfoItemEntry(
     term: String,
     description: String,
 ) {
-    Column {
-        Text(
-            text = term,
-            style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            text = description,
-        )
+    val clipboardManager = LocalClipboardManager.current
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Column(
+            modifier = Modifier.weight(0.90f)
+        ) {
+            Text(
+                text = term,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = description,
+            )
+        }
+        IconButton(
+            onClick = {
+                clipboardManager.setClip(ClipEntry(ClipData.newPlainText(term, description)))
+            },
+            modifier = Modifier.weight(0.10f)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.ContentCopy,
+                contentDescription = stringResource(R.string.account_info_item_copy_term, term)
+            )
+        }
     }
 }
 
