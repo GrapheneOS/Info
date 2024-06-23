28b744dbdc3523a3ef217eb9aa386b1346b5a3fd
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-06-23 13:56:58 -0700

    set top bar title maxLines to 1 and overflow with ellipsis

--- app/src/main/kotlin/app/grapheneos/info/InfoApp.kt
+++ app/src/main/kotlin/app/grapheneos/info/InfoApp.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.IntOffset
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
@@ -163,7 +164,9 @@ fun InfoApp(
                 title = {
                     Text(
                         text = stringResource(id = currentScreen.title),
-                        style = typography.headlineMedium
+                        style = typography.headlineMedium,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
                     )
                 },
                 navigationIcon = {
