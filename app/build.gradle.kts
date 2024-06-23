990da43dbde9e21289b20de918eed583e1cd57a9
Daniel Micay <daniel.micay@grapheneos.org>
2024-06-23 14:41:50 -0400

    remove extra newline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
