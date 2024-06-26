395b155574fa343d4117360aa1f15fa5e3ba1d0f
Daniel Micay <daniel.micay@grapheneos.org>
2024-06-26 16:07:06 -0400

    update AndroidX Compose UI library to 1.7.0-beta04

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -108,5 +108,5 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     // temporarily use beta until 1.7.0 stable releases with AnnotatedString.Companion.fromHtml
-    implementation("androidx.compose.ui:ui-text:1.7.0-beta03")
+    implementation("androidx.compose.ui:ui-text:1.7.0-beta04")
 }
