0e980242fdaec82054ade9799c4df68cc34214df
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-27 04:59:12 -0400

    update AndroidX Activity Compose to 1.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.navigation:navigation-compose:2.8.3")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
