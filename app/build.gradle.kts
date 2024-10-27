4a09c3315dbe1845c3c46d0908325763e2f2efe4
soupslurpr <92235850+soupslurpr@users.noreply.github.com>
2024-10-26 18:46:00 -0700

    add AndroidX Material 3 Adaptive Navigation Suite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,6 +107,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite")
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
     debugImplementation("androidx.compose.ui:ui-tooling")
