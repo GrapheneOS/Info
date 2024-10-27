726c5f416f6f51d5fd56a0bdc9746cc21b3b7e39
Daniel Micay <daniel.micay@grapheneos.org>
2024-10-27 05:03:01 -0400

    update NDK to 27.2.12479018

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
     namespace = "app.grapheneos.info"
     compileSdk = 35
     buildToolsVersion = "35.0.0"
-    ndkVersion = "27.1.12297006"
+    ndkVersion = "27.2.12479018"
 
     defaultConfig {
         applicationId = "app.grapheneos.info"
