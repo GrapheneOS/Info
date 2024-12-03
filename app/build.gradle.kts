61ac09d82ac085c2eeab5314c2b4038fe0190bca
Daniel Micay <daniel.micay@grapheneos.org>
2024-12-03 10:33:59 -0500

    update Kotlin to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ if (useKeystoreProperties) {
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
 }
 
 android {
