53b93d2e3853d49c7e861f4211e3f436e77dcb5c
Daniel Micay <daniel.micay@grapheneos.org>
2025-01-15 02:04:25 -0500

    add extra newlines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,16 +54,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
         buildConfig = true
     }
+
     androidResources {
         generateLocaleConfig = true
     }
+
     buildTypes {
         release {
             isMinifyEnabled = true
