2e4452eff26871a66323808b0f6012bd4ed884d2
Daniel Micay <daniel.micay@grapheneos.org>
2025-03-03 13:47:12 -0500

    update AndroidX Compose BOM to 2025.02.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -102,7 +102,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-process:2.8.7")
 
-    implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-text")
     implementation("androidx.compose.ui:ui-graphics")
