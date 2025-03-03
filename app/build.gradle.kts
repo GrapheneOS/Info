c9c3e015317c6081633096dd5674f6276ec7fba9
Daniel Micay <daniel.micay@grapheneos.org>
2025-03-03 13:51:13 -0500

    update AndroidX Navigation Compose to 2.8.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.9.3")
-    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.navigation:navigation-compose:2.8.8")
     implementation("androidx.datastore:datastore-preferences:1.1.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-process:2.8.7")
