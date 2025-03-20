ea09d42d328af591b6e02276200d59bab14e8e54
Daniel Micay <daniel.micay@grapheneos.org>
2025-03-20 18:52:20 -0400

    update AndroidX DataStore Preferences to 1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -98,7 +98,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.8.9")
-    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-process:2.8.7")
 
