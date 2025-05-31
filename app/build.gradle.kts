25bda0a0a5333562b174ad15520ef2fcb9624409
Daniel Micay <daniel.micay@grapheneos.org>
2025-05-30 21:01:50 -0400

    update AndroidX Lifecycle libraries to 2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,8 +99,9 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.navigation:navigation-compose:2.9.0")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-process:2.8.7")
+    val lifecycleVersion = "2.9.0"
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-process:$lifecycleVersion")
 
     implementation(platform("androidx.compose:compose-bom:2025.05.01"))
     implementation("androidx.compose.ui:ui")
