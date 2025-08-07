985164a5ddbace55f959c15ba0bf734bcbbfc42e
Daniel Micay <daniel.micay@grapheneos.org>
2025-08-06 21:04:09 -0400

    update AndroidX Navigation Compose to 2.9.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("androidx.navigation:navigation-compose:2.9.3")
     implementation("androidx.datastore:datastore-preferences:1.1.7")
     val lifecycleVersion = "2.9.0"
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
